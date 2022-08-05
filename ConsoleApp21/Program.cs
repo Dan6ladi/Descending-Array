58f4724570d7824a5d9bdf45fc0d6ab1d9a85629
Dan6ladi <dan6ladi@gmail.com>
2022-08-05 11:20:23 +0100

    Add project files.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] studentNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int no;
+            for(int i = 0; i < studentNumbers.Length - 1; i++)
+                for (int j = i + 1; j < studentNumbers.Length; j++)
+                    if (studentNumbers[i] < studentNumbers[j]) 
+                    { 
+                        no = studentNumbers[i];
+                        studentNumbers[i] = studentNumbers[j];
+                        studentNumbers[j] = no;
+                    
+                    }
+            foreach (int i in studentNumbers)   
+            Console.WriteLine(i + "  ");
+        }
+    }
+}
